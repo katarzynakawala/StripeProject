46e90d70c66462855089a6614f1f2ac26038e658
Katarzyna Kawala <kat.kawala@gmail.com>
2021-11-01 17:33:13 +0100

    adding errors handling and (un)marshaling

--- errors_test.go
+++ errors_test.go
@@ -18,9 +18,29 @@ var errorJSON = []byte(
 		}
 	}`)
 
-//func TestError_Marshal(t *testing.T) {
+func TestError_Marshal(t *testing.T) {
+	se := stripe.Error{
+		Code:    "test-code",
+		DocURL:  "test-docUrl",
+		Message: "test-message",
+		Param:   "test-param",
+		Type:    "test-type",
+	}
+	data, err := json.Marshal(se)
+	if err != nil {
+		t.Fatalf("Marshal() err = %v; want nil", err)
+	}
 
-//}
+	var got stripe.Error
+	err = json.Unmarshal(data, &got)
+	if err != nil {
+		t.Fatalf("Unmasrshal() err = %v; want nil", err)
+	}
+	if got != se {
+		t.Errorf("got = %v; want %v", got, se)
+		t.Log("Is Unmarshal working? It is required for this test to pass.")
+	}
+}
 
 func TestError_Unmarshal(t *testing.T) {
 	var se stripe.Error
@@ -29,17 +49,17 @@ func TestError_Unmarshal(t *testing.T) {
 		t.Fatalf("Unmarshal() err =%v; want nil", err)
 	}
 
-	wantDocUrl :=  "https://stripe.com/docs/error-codes/resource-missing"
+	wantDocUrl := "https://stripe.com/docs/error-codes/resource-missing"
 	if se.DocURL != wantDocUrl {
 		t.Errorf("DocURL = %s; want %s", se.DocURL, wantDocUrl)
 	}
 
-	wantType :=  "invalid_request_error"
+	wantType := "invalid_request_error"
 	if se.Type != wantType {
 		t.Errorf("Type = %s; want %s", se.Type, wantType)
 	}
 
-	wantMessage :=  "No such customer: 'cus_123'"
+	wantMessage := "No such customer: 'cus_123'"
 	if se.Message != wantMessage {
 		t.Errorf("Message = %s; want %s", se.Message, wantMessage)
 	}
